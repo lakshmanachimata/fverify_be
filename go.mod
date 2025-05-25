5ee08daad2d63a9477ae0092f1cc282b8870b6c8
Lakshmana <lakshman.chimata@gmail.com>
2025-05-25 21:32:33 +0530

    renamed model files

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module fverify_be
 
-go 1.24
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
 	github.com/gin-contrib/cors v1.7.5
@@ -8,6 +10,7 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/spf13/viper v1.20.1
+	github.com/stretchr/testify v1.10.0
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.4
@@ -15,13 +18,17 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
@@ -53,9 +60,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
---
+++ internal/models/organisation.go
@@ -0,0 +1,40 @@
+package models
+
+// OrganisationStatus represents the status of an organisation.
+// Enum: "Created", "Active", "InActive"
+type OrganisationStatus string
+
+const (
+	OrgCreated  OrganisationStatus = "Created"
+	OrgActive   OrganisationStatus = "Active"
+	OrgInActive OrganisationStatus = "InActive"
+)
+
+// OrganisationReq represents an Organisation Request in the system.
+// @Description OrganisationReq model containing all organisation request related information.
+//
+//	@Example {
+//	  "org_id": "12345",
+//	  "org_name": "Acme Corp",
+//	  "status": "Active"
+//	}
+type OrganisationReq struct {
+	OrgId   string             `json:"org_id" bson:"org_id" example:"12345"`         // Organisation ID
+	OrgName string             `json:"org_name" bson:"org_name" example:"Acme Corp"` // Organisation Name
+	Status  OrganisationStatus `json:"status" bson:"status" example:"Active"`        // Organisation Status
+}
+
+// Organisation represents an organisation in the system.
+// @Description Organisation model containing all organisation-related information.
+//
+//	@Example {
+//	  "org_id": "12345",
+//	  "org_name": "Acme Corp",
+//	  "status": "Active"
+//	}
+type Organisation struct {
+	OrgId   string             `json:"org_id" bson:"org_id" example:"12345"`         // Organisation ID
+	OrgName string             `json:"org_name" bson:"org_name" example:"Acme Corp"` // Organisation Name
+	OrgUUID string             `json:"org_uuid" bson:"org_uuid" example:"uuid-v4"`   // Auto-generated UUID
+	Status  OrganisationStatus `json:"status" bson:"status" example:"Active"`        // Organisation Status
+}
